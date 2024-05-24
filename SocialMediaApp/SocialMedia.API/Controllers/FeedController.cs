19e84405486cf5172c4d773cb1b74059c67cb753
Marcos Paulo <marcos.poo@gmail.com>
2024-05-24 17:03:00 -0300

    Implementando primeira lógica de Feed

---
+++ SocialMediaApp/SocialMedia.API/Controllers/FeedController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Application.Services.Feeds;
+
+namespace SocialMedia.API.Controllers
+{
+    [Route("api/feeds")]
+    [ApiController]
+    public class FeedController : Controller
+    {
+        private readonly IFeedService _feedService;
+
+        public FeedController(IFeedService feedService)
+        {
+            _feedService = feedService;
+        }
+
+        [HttpGet("Listagem/{idPerfil}")]
+        public IActionResult ObterFeed(int idPerfil)
+        {
+            var result = _feedService.GetAll(idPerfil);
+
+            return Ok(result);
+        }
+    }
+}
